742703a3ff15fc29aa9a30ba27f47bd8bbd4d891
hao <zhaoguohao@peilian.com>
2020-05-26 21:43:37 +0800

    不完整版Unity2D AStar(逆向寻路会有问题，路径问题有时候不是最优~)

---
+++ Assets/Scripts/AStar/AStarMgr.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// A星寻路管理器 单例模式
+/// </summary>
+public class AStarMgr
+{
+    //单例
+    private static AStarMgr instace;
+
+    public static AStarMgr Instance
+    {
+        get
+        {
+            if (instace == null)
+                instace = new AStarMgr();
+            return instace;
+
+        }
+    }
+
+    //地图的宽高
+    private int mapW;
+    private int mapH;
+
+    /// <summary>
+    /// 地图相关所有的格子对象容器
+    /// </summary>
+    public AStarNode[,] nodes;
+
+    /// <summary>
+    /// 开启列表
+    /// </summary>
+    private List<AStarNode> openList = new List<AStarNode>();
+
+    /// <summary>
+    /// 关闭列表
+    /// </summary>
+    private List<AStarNode> closeList = new List<AStarNode>();
+
+    /// <summary>
+    /// 初始化地图
+    /// </summary>
+    /// <param name="w"></param>
+    /// <param name="h"></param>
+    public void InitMapInfo(int w, int h)
+    {
+        this.mapW = w;
+        this.mapH = h;
+
+        nodes = new AStarNode[w, h];
+        for (int i = 0; i < w; ++i)
+        {
+            for (int j = 0; j < h; ++j)
+            {
+                AStarNode node = new AStarNode(i, j, Random.Range(0, 100) < 30 ? E_Node_Type.Stop : E_Node_Type.Walk);
+                nodes[i, j] = node;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 寻路方法 提供给外部
+    /// </summary>
+    /// <param name="starPos"></param>
+    /// <param name="endPos"></param>
+    /// <returns></returns>
+    public List<AStarNode> FindPath(Vector2 starPos, Vector2 endPos)
+    {
+        if (starPos.x < 0 || starPos.x >= mapW ||
+            starPos.y < 0 || starPos.y >= mapH ||
+            endPos.x < 0 || endPos.x >= mapW ||
+            endPos.y < 0 || endPos.y >= mapH)
+        {
+            Debug.Log("开始或者结束点在地图格子范围之外！！！");
+            return null;
+        }
+
+        AStarNode start = nodes[(int)starPos.x, (int)starPos.y];
+        AStarNode end = nodes[(int)endPos.x, (int)endPos.y];
+
+        if (start.type == E_Node_Type.Stop || end.type == E_Node_Type.Stop)
+
+        {
+            Debug.Log("开始或者结束点是阻挡！！！");
+            return null;
+        }
+
+        closeList.Clear();
+        openList.Clear();
+
+        start.father = null;
+        start.f = 0;
+        start.g = 0;
+        start.h = 0;
+        closeList.Add(start);
+
+        while (true)
+        {
+            FindNearlyNodeToOpenList(start.x - 1, start.y - 1, 1.4f, start, end);
+            FindNearlyNodeToOpenList(start.x, start.y - 1, 1, start, end);
+            FindNearlyNodeToOpenList(start.x + 1, start.y - 1, 1.4f, start, end);
+            FindNearlyNodeToOpenList(start.x - 1, start.y, 1, start, end);
+            FindNearlyNodeToOpenList(start.x + 1, start.y, 1, start, end);
+            FindNearlyNodeToOpenList(start.x - 1, start.y + 1, 1.4f, start, end);
+            FindNearlyNodeToOpenList(start.x, start.y + 1, 1, start, end);
+            FindNearlyNodeToOpenList(start.x + 1, start.y + 1, 1.4f, start, end);
+
+
+            if (openList.Count == 0)
+            {
+                Debug.Log("死路");
+                return null;
+            }
+
+            openList.Sort(SortOpneList);
+
+            closeList.Add(openList[0]);
+            start = openList[0];
+            openList.RemoveAt(0);
+
+            if (start == end)
+            {
+                List<AStarNode> path = new List<AStarNode>();
+                path.Add(end);
+                while (end.father != null)
+                {
+                    path.Add(end.father);
+                    end = end.father;
+                }
+                //列表翻转
+                path.Reverse();
+                return path;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 排序函数
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    private int SortOpneList(AStarNode a, AStarNode b)
+    {
+        if (a.f > b.f)
+        {
+            return 1;
+        }
+        else if(a.f==b.f)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+
+    }
+
+    private void FindNearlyNodeToOpenList(int x, int y, float g, AStarNode father, AStarNode end)
+    {
+        if (x < 0 || x >= mapW || y < 0 || y >= mapH)
+            return;
+
+        AStarNode node = nodes[x, y];
+
+        if (node == null || node.type == E_Node_Type.Stop ||
+         closeList.Contains(node) || openList.Contains(node))
+            return;
+        node.father = father;
+
+        node.g = father.g + g;
+        node.h = Mathf.Abs(end.x - node.x) + Mathf.Abs(end.y - node.y);
+        node.f = node.g + node.h;
+
+        openList.Add(node);
+    }
+}
---
+++ Assets/Scripts/TestAStar.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAStar : MonoBehaviour
+{
+
+    public int beginX = -3;
+    public int beginY = 5;
+
+    public int offsetX = -2;
+    public int offsetY = 2;
+
+    public int mapW = 5;
+    public int mapH = 5;
+
+    public Material red;
+
+    public Material yellow;
+
+    public Material green;
+
+    public Material white;
+    private Vector2 beginPos = Vector2.right * -1;
+
+    private List<AStarNode> list;
+    private Dictionary<string, GameObject> Cubes = new Dictionary<string, GameObject>();
+    // Use this for initialization
+    void Start()
+    {
+        AStarMgr.Instance.InitMapInfo(mapW, mapH);
+        for (int i = 0; i < mapW; ++i)
+        {
+            for (int j = 0; j < mapH; ++j)
+            {
+                GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                obj.transform.position = new Vector3(beginX + i * offsetX, beginY + j * offsetY, 0);
+                obj.name = i + "_" + j;
+                Cubes.Add(obj.name, obj);
+                AStarNode node = AStarMgr.Instance.nodes[i, j];
+                if (node.type == E_Node_Type.Stop)
+                {
+                    obj.GetComponent<MeshRenderer>().material = red;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit info;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out info, 1000))
+            {
+                if (beginPos == Vector2.right * -1)
+                {
+                    if (list != null)
+                    {
+                        for (int i = 0; i < list.Count; ++i)
+                        {
+                            Cubes[list[i].x + "_" + list[i].y].GetComponent<MeshRenderer>().material = white;
+                        }
+
+                    }
+                    string[] strs = info.collider.gameObject.name.Split('_');
+                    beginPos = new Vector2(int.Parse(strs[0]), int.Parse(strs[1]));
+                    info.collider.gameObject.GetComponent<MeshRenderer>().material = yellow;
+                }
+                else
+                {
+                    string[] strs = info.collider.gameObject.name.Split('_');
+                    Vector2 endPos = new Vector2(int.Parse(strs[0]), int.Parse(strs[1]));
+
+                    list = AStarMgr.Instance.FindPath(beginPos, endPos);
+
+                    Cubes[beginPos.x + "_" + beginPos.y].GetComponent<MeshRenderer>().material = white;
+
+                    if (list != null)
+                    {
+                        for (int i = 0; i < list.Count; ++i)
+                        {
+                            Cubes[list[i].x + "_" + list[i].y].GetComponent<MeshRenderer>().material = green;
+                        }
+                    }
+
+
+                    beginPos = Vector2.right * -1;
+
+                }
+            }
+        }
+    }
+}
